ccd82ca798df02bd524101b645ce21caa3ace0f9
nil168 <swapnanil52@gmail.com>
2022-01-28 20:02:55 +0530

    Data Structure and algorithms -- few singly linked list basic functions added

---
+++ src/datastructure/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,166 @@
+package datastructure.linkedlist;
+
+public class SinglyLinkedList {
+    private Listnode head;
+
+    private static class Listnode {
+        private int data; // * can be a generic data type
+        private Listnode next; // * reference to the next node of singly linked list
+
+        public Listnode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    // * method for inserting new nodes at the beginning of a linked list
+    public void addFirst(int value){
+        Listnode newnode = new Listnode(value);
+        newnode.next = head;
+        head = newnode;
+    }
+    // * method to delete the first node of the linked list
+    public Listnode deleteFirst(){
+        if(head == null){
+            return null;
+        } else {
+            Listnode temp = head;
+            head  = head.next;
+            temp.next = null;
+            return temp;
+        }
+    }
+
+    // * method for inserting new nodes at the beginning of a linked list
+    public void addLast(int value){
+        Listnode newnode = new Listnode(value);
+        if(head == null){
+            head = newnode;
+            return;
+        }
+        Listnode current = head;
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = newnode;
+    }
+    // * method for deleting the last node
+    public Listnode deleteLast(){
+        if(head == null || head.next == null){
+            return head;
+        } else{
+            Listnode previous = null;
+            Listnode current = head;
+            while(current.next != null){
+                previous = current;
+                current = current.next;
+            }
+            previous.next = null;
+            return current;
+        }
+    }
+
+    // * method for inserting a node at any position
+    public void addAny(int value, int position){
+        Listnode newnode = new Listnode(value);
+        if(position == 1){
+            newnode.next = head;
+            head = newnode;
+        } else {
+            Listnode previous = head;
+            int count = 1;
+            while(count<position-1){
+                previous = previous.next;
+                count++;
+            }
+            Listnode current = previous.next;
+            previous.next = newnode;
+            newnode.next = current;
+        }
+    }
+    // * method for deleting a node at any position
+    public Listnode deleteAny(int position){
+        if(position == 1){
+            deleteFirst();
+        } else{
+            Listnode current = head;
+            Listnode previous = null;
+            int count = 1;
+            while(count<position-1){
+                previous = current;
+                current = current.next;
+                count++;
+            }
+            previous.next = current.next;
+            current.next = null;
+            return current;
+        }
+        return null;
+    }
+
+    // * Linkedlist display method
+    public void display(){
+        Listnode current = head;
+        while(current != null){
+            System.out.print(current.data+" --> ");
+            current = current.next;
+        }
+        System.out.print("null");
+    }
+
+    //  * Length Method
+    public int length(){
+        if(head == null){
+            return 0;
+        }
+        int count = 0;
+        Listnode current = head;
+        while(current != null){
+            count++;
+            current =current.next;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+
+        //  * creating the nodes
+        sll.head = new Listnode(11);
+        Listnode second = new Listnode(12);
+        Listnode third = new Listnode(13);
+        Listnode fourth = new Listnode(14);
+
+        //  * connecting the nodes
+        sll.head.next = second; // 11 --> 12
+        second.next = third; // 11 --> 12 --> 13
+        third.next = fourth; // 11 --> 12 --> 13 --> 14
+
+        // * display method call
+        sll.display();
+
+        //  * Length method call
+        System.out.println("\nThe Length of the Linked list is : "+sll.length());
+
+        // * adding new nodes
+        sll.addFirst(10);
+        sll.addFirst(9);
+        sll.addFirst(8);
+        sll.addLast(15);
+        sll.addLast(16);
+        sll.addAny(7,1);
+        sll.addAny(17,9);
+
+        sll.display();
+        System.out.println("\nThe Length of the Linked list is : "+sll.length());
+
+        System.out.println("the deleted first node : "+sll.deleteFirst().data);
+        sll.display();
+
+        System.out.println("\nthe deleted last node : "+sll.deleteLast().data);
+        sll.display();
+
+        System.out.println("\nThe deleted node is : "+sll.deleteAny(9).data);
+        sll.display();
+
+    }
+}
